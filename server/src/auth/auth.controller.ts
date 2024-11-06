1fa507728107362a39c26eaee7e7ed9ede3b21d1
Barış Yesari <yesaribaris23@gmail.com>
2024-11-06 14:28:35 +0300

    return tokens with cookies

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/auth.dto';
 import { CreateUserDto } from 'src/user/dto/createUser.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -10,13 +11,39 @@ export class AuthController {
 
     @Public()
     @Post('login')
-    async login(@Body() loginDto: LoginDto) {
-        return this.authService.login(loginDto);
+    async login(@Res({ passthrough: true }) res: Response, @Body() loginDto: LoginDto) {
+        const tokens = await this.authService.login(loginDto);
+        res.cookie('accessToken', tokens.access_token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            // maxAge: 15 * 60 * 1000
+        });
+        res.cookie('refreshToken', tokens.refresh_token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            // maxAge: 7 * 24 * 60 * 60 * 1000 
+        });
+        return {message:'Login successful!'}
     }
-    
+
     @Public()
     @Post('register')
-    async register(@Body() createUserDto: CreateUserDto) {
-        return this.authService.register(createUserDto)
+    async register(@Res({ passthrough: true }) res: Response,@Body() createUserDto: CreateUserDto) {
+        const tokens = await this.authService.register(createUserDto);
+        res.cookie('accessToken', tokens.access_token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            // maxAge: 15 * 60 * 1000
+        });
+        res.cookie('refreshToken', tokens.refresh_token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            // maxAge: 7 * 24 * 60 * 60 * 1000 
+        });
+        return {message:'Register successful!'}
     }
 }
